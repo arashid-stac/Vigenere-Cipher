234304b21a8fc9f68bd71294936aa1ad5b07e457
arashid-stac <allirashid97@gmail.com>
2019-08-19 10:39:18 -0400

    Changes
    1) Cipher now correctly handles spaces.
    2) All upper case letters are converted to lower case.

--- Vigenere Cipher/src/Coding.java
+++ Vigenere Cipher/src/Coding.java
@@ -17,10 +17,10 @@ public class Coding {
 	char[] encodedMessage;
 
 	private void encode() throws IOException {
-		// https://stackoverflow.com/questions/13878437/how-to-fill-an-array-of-characters-from-user-input
+		System.out.println("Welcome to the Vigenere Cipher program!");
 		br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter the message you'd like to encode.");
-		message = br.readLine();
+		System.out.println("Enter the message you'd like to encode. Symbols and numbers will be ignored.");
+		message = br.readLine().toLowerCase();
 		messageLength = message.length();
 		messageArray = new char[messageLength];
 		message.getChars(0, messageLength, messageArray, 0);
@@ -28,16 +28,17 @@ public class Coding {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		do {
 			System.out.println("Enter the keyword you'd like to use to encode your message.");
-			keyword = br.readLine();
+			keyword = br.readLine().toLowerCase();
 			keywordLength = keyword.length();
 			keywordArray = new char[keywordLength];
 			keyword.getChars(0, keywordLength, keywordArray, 0);
 			if (keywordLength > messageLength || keywordLength == 0) {
-				System.out.println("Please make sure the keyword length is less than the message length.");
+				System.out.println(
+						"Please make sure the keyword is one word and that its length is less than the message length.");
 			}
-		} while (keywordLength > messageLength || keywordLength == 0);
+		} while (keywordLength > messageLength || keywordLength == 0 || keyword.contains(""));
 
-		// If the keyword's length is shorter than that of the message, then loop the
+		// If the keyword's length is shorter than that of the message, loop the
 		// keyword back until the two are of equal length
 		if (keywordLength < messageLength) {
 			keywordRedone = new char[messageLength];
@@ -57,6 +58,26 @@ public class Coding {
 		// encode the message
 		int rowIndex = 0;
 		int columnIndex = 0;
+		/*
+		 * decrement handles the possibility of trying to encode a message that contains
+		 * spaces if there are spaces, the row and column indices will return 0,
+		 * therefore the next value in the encoded message will be a space when this
+		 * happens, the decrement increases by 1, which lets the keyword array to go
+		 * back one spot so that the letter that was skipped because of the resulting
+		 * space is still used.
+		 * 
+		 * For example: Let's say you want to encode the phrase "hi sir" using the
+		 * keyword "man". Normally, the way this would be encoded without the decrement
+		 * would go as follows: "h" would be encoded with "m", "i" would be encoded with
+		 * "a", and the "space" together with the "n" would be skipped because of how
+		 * the loop is set up. With the decrement, it lets the loop know that if a space
+		 * was hit in the last iteration, it should go back and still use the letter
+		 * from the keyword that was skipped. Therefore "s" would still be encoded with
+		 * the initially skipped keyword character "n".
+		 */
+
+		int decrement = 0;
+
 		for (int i = 0; i < messageLength; i++) {
 			// look for the correct indices
 			rowIndex = 0;
@@ -65,13 +86,23 @@ public class Coding {
 			for (int j = 0; j < 27; j++) {
 				if (theCipher[j][0] == messageArray[i]) {
 					// save the index
-					rowIndex = j;
-					break;
+					if (messageArray[i] == theCipher[0][0]) {
+						rowIndex = 0;
+
+					} else {
+						rowIndex = j;
+						break;
+					}
 				}
 			}
 			for (int k = 0; k < 27; k++) {
-				if (theCipher[0][k] == keywordArray[i]) {
+				if (theCipher[0][k] == keywordArray[i - decrement]) {
 					// save the index
+					if (rowIndex == 0) {
+						columnIndex = 0;
+						decrement++;
+						break;
+					}
 					columnIndex = k;
 					break;
 				}
@@ -79,6 +110,7 @@ public class Coding {
 
 			encodedMessage[i] = theCipher[rowIndex][columnIndex];
 		}
+		System.out.println("Here is your encoded message: ");
 		System.out.println(encodedMessage);
 	}
 
